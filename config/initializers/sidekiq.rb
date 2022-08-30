a5053b12d3da30461597c4ffe5aece0e52dc3a40
Mohamed Kamal <mkamal@inmobly.com>
2022-08-30 23:38:40 +0200

    update sidekiq host name to map to container host insteed

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 require 'sidekiq'
 
 Sidekiq.configure_server do |config|
-  config.redis = {url: 'redis://localhost:6379/0'}
+  config.redis = {url: 'redis://redis:6379/0'}
   schedule_file = "config/schedule.yml"
   if File.exists?(schedule_file)
     Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
@@ -9,5 +9,5 @@ Sidekiq.configure_server do |config|
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = {url: 'redis://localhost:6379/0'}
+  config.redis = {url: 'redis://redis:6379/0'}
 end
